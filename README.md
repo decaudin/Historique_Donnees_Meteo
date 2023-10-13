d2fee4405fee608f50699610328b35be0fbc4627
apprenti_développeur <xd.decaudin@gmail.com>
2023-10-13 13:25:21 +0200

    factorisation composant compare

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ Bonjour et bienvenue sur le code d'une application météo de comparaison de cer
 
 ensoleillement) entre différentes villes françaises et d'évolution de ces données de l'année 2000 à nos jours.
 
-Pour installer l'application, ouvrez un terminal et clonez le repository en tapant la commande git clone "URLrepo". Tapez ensuite npm install afin
+Pour installer l'application, ouvrez un terminal et clonez le repository en tapant la commande git clone https://github.com/decaudin/
 
-d'installer les dépendances nécessaires au bon fonctionnement du site.
+Historique_Donnees_Meteo.git. Tapez ensuite npm install afin d'installer les dépendances nécessaires au bon fonctionnement du site.
 
 Vous pouvez ensuite tapez npm start pour lancer l'application.
 
--- src/components/Compare/Compare.jsx
+++ src/components/Compare/Compare.jsx
@@ -2,20 +2,24 @@ import "./Compare.scss";
 import React, { useState } from 'react';
 
 const Compare = () => {
-   const [selectedPeriod, setSelectedPeriod] = useState('');
-   const [formDayVisible, setFormDayVisible] = useState(false);
-   const [formMonthVisible, setFormMonthVisible] = useState(false);
-   const [formYearVisible, setFormYearVisible] = useState(false);
-   const [selectedYear, setSelectedYear] = useState('');
-   const [selectedMonth, setSelectedMonth] = useState('');
-   const [selectedDay, setSelectedDay] = useState('');
+  const [selectedPeriod, setSelectedPeriod] = useState('');
+  const [selectedYear, setSelectedYear] = useState('');
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [selectedDay, setSelectedDay] = useState('');
+  const [formVisibility, setFormVisibility] = useState({
+    day: false,
+    month: false,
+    year: false,
+  });
 
-   const handlePeriodChange = (e) => {
+  const handlePeriodChange = (e) => {
     const selectedValue = e.target.value;
     setSelectedPeriod(selectedValue);
-    setFormDayVisible(selectedValue === '1');
-    setFormMonthVisible(selectedValue === '2');
-    setFormYearVisible(selectedValue === '3');
+    setFormVisibility({
+      day: selectedValue === '1',
+      month: selectedValue === '2',
+      year: selectedValue === '3',
+    });
   };
 
   const handleYearChange = (e) => {
@@ -26,119 +30,117 @@ const Compare = () => {
     setSelectedMonth(e.target.value);
   };
 
-  const generateDayOptions = () => {
-    const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
-    const dayOptions = [];
-  
-    for (let day = 1; day <= daysInMonth; day++) {
-      dayOptions.push(
-        <option key={day} value={day}>
-          {day}
+  const generateOptions = (start, end, formatter) => {
+    return Array.from({ length: end - start + 1 }, (_, index) => {
+      const value = start + index;
+      return (
+        <option key={value} value={value}>
+          {formatter(value)}
         </option>
       );
-    }
-  
-    return dayOptions;
+    });
+  };
+
+  const generateDayOptions = () => {
+    const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+    return generateOptions(1, daysInMonth, (day) => day);
+  };
+
+  const generateYearOptions = () => {
+    return generateOptions(2000, 2023, (year) => year);
+  };
+
+  const generateMonthOptions = () => {
+    return generateOptions(1, 12, (month) =>
+      new Date(2000, month - 1).toLocaleString('default', { month: 'long' })
+    );
   };
 
   return (
     <div className="compare">
-        <h3>Bienvenue sur l'outil de comparaison des données.</h3>
-        <form className="chooseType">
-            <label htmlFor="typeOfPeriod" className="period">Choisissez sur quel type de période va porter votre comparaison</label>
-            <select id="typeOfPeriod" onChange={handlePeriodChange} value={selectedPeriod}>
+      <h3>Bienvenue sur l'outil de comparaison des données.</h3>
+      <form className="chooseType">
+        <label htmlFor="typeOfPeriod" className="period">
+          Choisissez sur quel type de période va porter votre comparaison
+        </label>
+        <select id="typeOfPeriod" onChange={handlePeriodChange} value={selectedPeriod}>
+          <option value=""></option>
+          <option value="1">jour</option>
+          <option value="2">mois</option>
+          <option value="3">année</option>
+        </select>
+
+        {formVisibility.day && (
+          <div className="chooseDay">
+            <div className="year">
+              <label htmlFor="fineYear">Choisissez l'année</label>
+              <select id="fineYear" onChange={handleYearChange}>
                 <option value=""></option>
-                <option value="1">jour</option>
-                <option value="2">mois</option>
-                <option value="3">année</option>
-            </select>
-
-            {formDayVisible && (
-              <div className="chooseDay">
-                <div className="year">
-                  <label htmlFor="fineYear">Choisissez l'année</label>
-                  <select id="fineYear" onChange={handleYearChange}>
-                    <option value=""></option>
-                    {Array.from({ length: 24 }, (_, index) => {
-                      const year = 2000 + index;
-                      return <option key={year} value={year}>{year}</option>;
-                    })}
-                  </select>
-                </div>
-                <div className="month">
-                  <label htmlFor="fineMonth">Choisissez le mois</label>
-                  <select id="fineMonth" onChange={handleMonthChange}>
-                    <option value=""></option>
-                    {Array.from({ length: 12 }, (_, index) => {
-                      const month = index + 1;
-                      const monthName = new Date(2000, index).toLocaleString('default', { month: 'long' });
-                      return <option key={month} value={month}>{monthName}</option>;
-                    })}
-                  </select>
-                </div>
-                <div className="day">
-                  <label htmlFor="fineDay">Choisissez le jour</label>
-                  <select id="fineDay" onChange={(e) => setSelectedDay(e.target.value)}>
-                    <option value=""></option>
-                    {generateDayOptions()}
-                  </select>
-                </div>
-              </div>
-            )}
-
-            {formMonthVisible && (
-              <div className="chooseMonth">
-                <div className="year">
-                  <label htmlFor="fineYear">Choisissez l'année</label>
-                  <select id="fineYear" onChange={handleYearChange}>
-                    <option value=""></option>
-                    {Array.from({ length: 24 }, (_, index) => {
-                      const year = 2000 + index;
-                      return <option key={year} value={year}>{year}</option>;
-                    })}
-                  </select>
-                </div>
-                <div className="month">
-                  <label htmlFor="fineMonth">Choisissez le mois</label>
-                  <select id="fineMonth" onChange={handleMonthChange}>
-                    <option value=""></option>
-                    {Array.from({ length: 12 }, (_, index) => {
-                      const month = index + 1;
-                      const monthName = new Date(2000, index).toLocaleString('default', { month: 'long' });
-                      return <option key={month} value={month}>{monthName}</option>;
-                    })}
-                  </select>
-                </div>
-              </div>
-            )}
-
-            {formYearVisible && (
-              <div className="chooseYear">        
-                <label htmlFor="fineYear">Choisissez l'année</label>
-                <select id="fineYear" onChange={handleYearChange}>
-                  <option value=""></option>
-                  {Array.from({ length: 24 }, (_, index) => {
-                    const year = 2000 + index;
-                    return <option key={year} value={year}>{year}</option>;
-                  })}
-                </select>
-              </div>
-            )}
-
-            <label htmlFor="typeOfData" className="data">Choisissez les données que vous souhaitez comparer</label>
-            <select id="typeOfData">
+                {generateYearOptions()}
+              </select>
+            </div>
+            <div className="month">
+              <label htmlFor="fineMonth">Choisissez le mois</label>
+              <select id="fineMonth" onChange={handleMonthChange}>
                 <option value=""></option>
-                <option value="1">Température minimale [C°]</option>
-                <option value="2">Température maximale [C°]</option>
-                <option value="3">Température moyenne [C°]</option>
-                <option value="4">Vent maximal [km/h]</option>
-                <option value="5">Ensoleillement [h]</option>
-                <option value="6">Précipitations totales [mm]</option>
+                {generateMonthOptions()}
+              </select>
+            </div>
+            <div className="day">
+              <label htmlFor="fineDay">Choisissez le jour</label>
+              <select id="fineDay" onChange={(e) => setSelectedDay(e.target.value)}>
+                <option value=""></option>
+                {generateDayOptions()}
+              </select>
+            </div>
+          </div>
+        )}
+
+        {formVisibility.month && (
+          <div className="chooseMonth">
+            <div className="year">
+              <label htmlFor="fineYear">Choisissez l'année</label>
+              <select id="fineYear" onChange={handleYearChange}>
+                <option value=""></option>
+                {generateYearOptions()}
+              </select>
+            </div>
+            <div className="month">
+              <label htmlFor="fineMonth">Choisissez le mois</label>
+              <select id="fineMonth" onChange={handleMonthChange}>
+                <option value=""></option>
+                {generateMonthOptions()}
+              </select>
+            </div>
+          </div>
+        )}
+
+        {formVisibility.year && (
+          <div className="chooseYear">
+            <label htmlFor="fineYear">Choisissez l'année</label>
+            <select id="fineYear" onChange={handleYearChange}>
+              <option value=""></option>
+              {generateYearOptions()}
             </select>
-            <input type="submit" className="submitButton" value="Valider" />    
-        </form>
+          </div>
+        )}
+
+        <label htmlFor="typeOfData" className="data">
+          Choisissez les données que vous souhaitez comparer
+        </label>
+        <select id="typeOfData">
+          <option value=""></option>
+          <option value="1">Température minimale [C°]</option>
+          <option value="2">Température maximale [C°]</option>
+          <option value="3">Température moyenne [C°]</option>
+          <option value="4">Vent maximal [km/h]</option>
+          <option value="5">Ensoleillement [h]</option>
+          <option value="6">Précipitations totales [mm]</option>
+        </select>
+        <input type="submit" className="submitButton" value="Valider" />
+      </form>
     </div>
   );
-}
+};
 
 export default Compare;
